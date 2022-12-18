e226f1691f3fac76b43749ce202c07370d43ba23
yladev <yanlinnaung881@gmail.com>
2022-12-18 19:34:41 +0630

    contact us page done

---
+++ form-handler.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@Andrew_Programming.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = 'User Name: $name.'
+
+
+
+
+
+
+
+?>
